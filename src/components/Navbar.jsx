36544321a6108539734441f49d8a6b87f749f947
wdthor <wdthor@proton.me>
2024-11-09 02:35:02 +0100

    fix image url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
+import logo from "../assets/react.svg";
 export function Navbar() {
   return (
     <nav className="flex justify-between items-center py-8 px-16 bg-grey-800">
       <section className="flex items-center gap-8">
-        <img src="../src/assets/react.svg" alt="logo" />
+        <img src={logo} alt="logo" />
         <h2 className="text-primary">React Facts</h2>
       </section>
       <h3 className="text-grey-100">React Course - Project 1</h3>
